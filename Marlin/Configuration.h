70cc8e55d26c4a4d165b74d872eca27e954441eb
Fabrizio <fabiecatinella@gmail.com>
2024-03-25 15:41:41 +0000

    Update BLTOUCH nozzle distance to support new hotend cooling assembly.

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1659,7 +1659,7 @@
  *     O-- FRONT --+
  */
 #define NOZZLE_TO_PROBE_OFFSET \
-    { -44, -17, -2.28 }
+    { -42, -10, -3.05 }  // Fabie37
 
 // Enable and set to use a specific tool for probing. Disable to allow any tool.
 #define PROBING_TOOL 0
